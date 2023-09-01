0ab2bde29fbdce32245a1f5c2ec40edd975e3949
attahchisom6 <attahchisom6@gmail.com>
2023-09-01 18:30:22 +0000

    add

---
+++ utils/redis.js
@@ -0,0 +1,43 @@
+import redis from 'redis'
+import promisify from 'util':
+
+class RedisClient {
+  constructor() {
+    this.hasConnected = true;
+    this.client = redis.createClient();
+    this.client.on('error', (error) => {
+      console.error(error);
+      this.hasConnected = false;
+    })
+    .on('connect', () => {
+      this.client.hasConnected = true;
+    });
+  }
+
+  isAlive() {
+    return this.hasConnected;
+  }
+
+  async set(key, value, expirationDuration) {
+    //setex for setting with expiration duration
+    const asyncSet = promidify(this.client.setex).bind(this.client);
+    await asyncSet(key, value, expirationDuration);
+  }
+
+  async get(key) {
+    const asyncGet = promisify(this.client.get).bind(this.client);
+    const result = await asyncGet(key);
+    return result;
+  }
+
+  async del(key) {
+    const asyncDel = promisify(this.client.del).bind(this.client);
+    await asyncDel(key, () => {
+      console.log('Successfully deleted value with key:', key);
+    });
+  }
+}
+
+const redisClient = RedisClient;
+
+module.exports = redisClient();
