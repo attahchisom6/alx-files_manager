57b6931aae8273983e3cad9ae983e346c3212f9d
attahchisom6 <attahchisom6@gmail.com>
2023-09-02 20:23:27 +0000

    add

--- server.js
+++ server.js
@@ -1,21 +1,12 @@
 import express from 'express';
-import routes from '../routes/index.js';
+import routes from './routes/index.js';
 
 const app = express();
-const router = app.use('/', route);
-const port = process.env.PORT || env;
-
-app.get('/status', (req, res) => {
-  res.200.send(AppController.getStatus);
-})
-
-app.get('/stats', (req, res) => {
-  res.200.send('/AppController.getStats');
-});
+app.use(express.json());
+app.use('/', routes);
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-  console(`server running and listening on port ${port}`);
+  console.log(`server running and listening on port ${port}`);
 });
-
-module.exports = app;
 
