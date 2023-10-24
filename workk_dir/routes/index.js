d042b5a18024b9e70289f9a4d51c45774b206d5b
attahchisom6 <attahchisom@gmail.com>
2023-10-24 23:11:39 +0100

    controllers/

--- workk_dir/routes/index.js
+++ workk_dir/routes/index.js
@@ -24,7 +24,9 @@ router.post('/files', FilesController.postUpload);
 router.get('/files/:id', FilesController.getShow);
 router.get('/files', FilesController.getIndex);
 
-router.put('/files/:id/putPublish', FilesController.putPublish);
+router.put('/files/:id/publish', FilesController.putPublish);
 router.put('/files/:id/unpublish', FilesController.putUnpublish);
+
+router.get('/files/:id/data', FilesController.getFile);
 
 export default router;
